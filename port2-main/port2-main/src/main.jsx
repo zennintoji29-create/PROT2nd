39a276153e56a3aaf9a9f5c0ecc46c8655cbe1fd
zennintoji29 <zennintoji29@gmail.com>
2025-11-27 16:44:24 +0530

    Update main.jsx

--- port2-main/port2-main/src/main.jsx
+++ port2-main/port2-main/src/main.jsx
@@ -1,11 +1,7 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App'
-import './styles/global.css'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./styles/global.css";
 
-
-createRoot(document.getElementById('root')).render(
-<React.StrictMode>
-<App />
-</React.StrictMode>
-)
+const root = createRoot(document.getElementById("root"));
+root.render(<App />);
