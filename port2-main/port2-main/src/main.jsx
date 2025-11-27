74936bca5d8b756f7d2ded2dd3a01a20e9b15223
zennintoji29 <zennintoji29@gmail.com>
2025-11-27 17:16:49 +0530

    Update main.jsx

--- port2-main/port2-main/src/main.jsx
+++ port2-main/port2-main/src/main.jsx
@@ -1,1 +1,10 @@
-createRoot(document.getElementById('root')).render( <React.StrictMode> <App /> </React.StrictMode> )
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css"; // Make sure this exists
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
