37eccab915657fc64e84818cc9189343a87ef2f4
zennintoji29 <zennintoji29@gmail.com>
2025-11-27 16:25:10 +0530

    Update skills.js

--- port2-main/port2-main/src/data/skills.js
+++ port2-main/port2-main/src/data/skills.js
@@ -1,8 +1,46 @@
 const skills = {
-frontend: ['React', 'JavaScript (ES6+)', 'TypeScript (basic)', 'Vite', 'Tailwind CSS', 'Framer Motion'],
-tools: ['Git', 'GitHub', 'VSCode', 'Figma'],
-others: ['Unit Testing', 'Responsive Design', 'Accessibility']
-}
+  frontend: [
+    "React",
+    "JavaScript (ES6+)",
+    "TypeScript (Basic)",
+    "Vite",
+    "Tailwind CSS",
+    "Framer Motion"
+  ],
+  tools: [
+    "Git",
+    "GitHub",
+    "VS Code",
+    "Figma",
+    "npm",
+    "Postman"
+  ],
+  programming: [
+    "Java",
+    "Python",
+    "JavaScript",
+    "HTML ",
+    "CSS",
+    "Node.js (Basics)",
+  ],
+  softSkills: [
+    "Communication",
+    "Team Collaboration",
+    "Problem-Solving",
+    "Creativity",
+    "Adaptability",
+    "Leadership (Emerging)",
+    "Time Management",
+    "Critical Thinking"
+  ],
+  other: [
+    "Unit Testing",
+    "Responsive Design",
+    "Accessibility",
+    "Prompt Engineering",
+    "API Integration (Basic)",
+    "Open-Source Contribution",
+  ]
+};
 
-
-export default skills
+export default skills;
